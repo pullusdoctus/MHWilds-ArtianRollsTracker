65720ff2c778f610e7501e7421ae2530fd1a6d4c
pullusdoctus <ja.rojas.dev@gmail.com>
2025-10-07 00:23:57 -0600

    init

---
+++ src/App/__init__.py
@@ -0,0 +1,5 @@
+from GUI import GUI
+
+class App:
+    def __init__(self):
+        gui = GUI()
---
+++ src/GUI/__init__.py
@@ -0,0 +1,5 @@
+import flet
+
+class GUI:
+    def __init__(self):
+        print("NOCHES DE FANTASIAAAAAAAAAAAAAA")
